24689f0631d29837576c4aefe51d5967e435a163
Marko Tagoma <markoets1@gmail.com>
2022-09-09 15:17:19 +0300

    Add /tickets endpoint and Update swagger.json [Delivers #183186560]

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = 8080
 const swaggerUi = require ('swagger-ui-express')
 const swaggerDocument = require ('./docs/swagger.json');
 
+app.get('/tickets',(req,res)=>{
+    res.send(["Concerts","Opera"])
+})
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port,()=>{
