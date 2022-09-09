1f7a549431720368a6503cf4099bf39408f6141b
Marko Tagoma <markoets1@gmail.com>
2022-09-09 15:53:39 +0300

    Add /locations endpoint and Update swagger.json [Delivers #183186601]

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get('/tickets',(req,res)=>{
     res.send(["Concerts","Opera"])
 })
 
+app.get('/locations',(req,res)=>{
+    res.send(["Tallinn","Tartu"])
+})
+
+
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port,()=>{
