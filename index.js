cc37cc6b72febbe8ec429501c9d34fcece90f98f
Henry Lehis <lehishenry1@gmail.com>
2022-09-13 13:27:17 +0300

    Add endpoint to get tickets,  actors, locations details

--- index.js
+++ index.js
@@ -1,71 +1,106 @@
 const app = require('express')()
 const port = 8080
-const swaggerUi = require ('swagger-ui-express')
-const swaggerDocument = require ('./docs/swagger.json');
+const swaggerUi = require('swagger-ui-express')
+const yamljs = require('yamljs')
+const swaggerDocument = yamljs.load('./docs/swagger.yaml')
 
 const tickets = [
-    {id: 1, name: "Top gun", cast: "Tom Cruise, Jennifer Connelly, Jon Hamm", price: 15.99},
-    {id: 2, name: "The Invitation", cast: "Nathalie Emmanuel, Thomas Doherty", price: 13.99},
-    {id: 3, name: "Avatar", cast: "Sam Worthington, Zoe Saldana, Sigourney Weaver", price: 10.99},
-    {id: 4, name: "André Rieu in Dublin", cast: "André Rieu", price: 25.99},
-    {id: 5, name: "Cinema Classics: Goodfellas", cast: "Robert De Niro, Joe Pesci, Ray Liota", price: 15.99},
-    {id: 6, name: "Beast", cast: "Idris Elba, Sharlto Copley", price: 19.99},
-    {id: 7, name: "Bullet Train", cast: "Brad Pitt, Aaron Taylor-Johnson, Sandra Bullock, Hiroyuki Sanada", price: 11.99},
-    {id: 8, name: "About Fate", cast: "Emma Roberts, Thomas Mann", price: 18.99},
-    {id: 9, name: "After Ever Happy", cast: "Hero Fiennes Tiffin, Josephine Langford, Louise Lombard", price: 16.99},
-    {id: 10, name: "Nope", cast: "Barbie Ferreira, Keke Palmer, Michael Wincott", price: 12.99},
+    { id: 1, name: "Top gun", cast: "Tom Cruise, Jennifer Connelly, Jon Hamm", price: 15.99 },
+    { id: 2, name: "The Invitation", cast: "Nathalie Emmanuel, Thomas Doherty", price: 13.99 },
+    { id: 3, name: "Avatar", cast: "Sam Worthington, Zoe Saldana, Sigourney Weaver", price: 10.99 },
+    { id: 4, name: "André Rieu in Dublin", cast: "André Rieu", price: 25.99 },
+    { id: 5, name: "Cinema Classics: Goodfellas", cast: "Robert De Niro, Joe Pesci, Ray Liota", price: 15.99 },
+    { id: 6, name: "Beast", cast: "Idris Elba, Sharlto Copley", price: 19.99 },
+    { id: 7, name: "Bullet Train", cast: "Brad Pitt, Aaron Taylor-Johnson, Sandra Bullock, Hiroyuki Sanada", price: 11.99 },
+    { id: 8, name: "About Fate", cast: "Emma Roberts, Thomas Mann", price: 18.99 },
+    { id: 9, name: "After Ever Happy", cast: "Hero Fiennes Tiffin, Josephine Langford, Louise Lombard", price: 16.99 },
+    { id: 10, name: "Nope", cast: "Barbie Ferreira, Keke Palmer, Michael Wincott", price: 12.99 },
 ]
 
 const locations = [
-    {id: 1, country: "Estonia", city: "Tallinn", street:"Solaris"},
-    {id: 2, country: "Estonia", city: "Pärnu", street: "Saku"},
-    {id: 3, country: "Estonia", city: "Tartu", street: "Ringtee"},
-    {id: 4, country: "Estonia", city: "Saaremaa", street:"Kudjape"},
-    {id: 5, country: "Estonia", city: "Jõhvi", street: "Puru tee"},
-    {id: 6, country: "Estonia", city: "Narva", street: "Tallinna mnt"},
-    {id: 7, country: "Estonia", city: "Tallinn", street: "Hobujaam"},
-    {id: 8, country: "Estonia", city: "Tallinn", street: "Mustamäe"},
-    {id: 9, country: "Estonia", city: "Tallinn", street: "Ülemsite"},
-    {id: 10, country: "Estonia", city: "Tallinn", street: "Endla"},
+    { id: 1, country: "Estonia", city: "Tallinn", street: "Solaris" },
+    { id: 2, country: "Estonia", city: "Pärnu", street: "Saku" },
+    { id: 3, country: "Estonia", city: "Tartu", street: "Ringtee" },
+    { id: 4, country: "Estonia", city: "Saaremaa", street: "Kudjape" },
+    { id: 5, country: "Estonia", city: "Jõhvi", street: "Puru tee" },
+    { id: 6, country: "Estonia", city: "Narva", street: "Tallinna mnt" },
+    { id: 7, country: "Estonia", city: "Tallinn", street: "Hobujaam" },
+    { id: 8, country: "Estonia", city: "Tallinn", street: "Mustamäe" },
+    { id: 9, country: "Estonia", city: "Tallinn", street: "Ülemsite" },
+    { id: 10, country: "Estonia", city: "Tallinn", street: "Endla" },
 ]
 
 const actors = [
-    {id: 1, name: "Tom Cruise", gender: "Male",age: 60},
-    {id: 2, name: "Emma Roberts", gender: "Female",age: 31},
-    {id: 3, name: "Brad pit", gender: "Male",age: 58},
-    {id: 4, name: "Thomas Doherty", gender: "Male",age: 27},
-    {id: 5, name: "Zoe Saldana", gender: "Female",age: 44},
-    {id: 6, name: "Thomas Mann", gender: "Male",age: 30},
-    {id: 7, name: "Jon Hamm", gender: "Male",age: 51},
-    {id: 8, name: "Robert De niro", gender: "Male",age: 79},
-    {id: 9, name: "Ray Liotta", gender: "Male",age: 67},
-    {id: 10, name: "Joe Pesci", gender: "Male",age: 79},
+    { id: 1, name: "Tom Cruise", gender: "Male", age: 60 },
+    { id: 2, name: "Emma Roberts", gender: "Female", age: 31 },
+    { id: 3, name: "Brad pit", gender: "Male", age: 58 },
+    { id: 4, name: "Thomas Doherty", gender: "Male", age: 27 },
+    { id: 5, name: "Zoe Saldana", gender: "Female", age: 44 },
+    { id: 6, name: "Thomas Mann", gender: "Male", age: 30 },
+    { id: 7, name: "Jon Hamm", gender: "Male", age: 51 },
+    { id: 8, name: "Robert De niro", gender: "Male", age: 79 },
+    { id: 9, name: "Ray Liotta", gender: "Male", age: 67 },
+    { id: 10, name: "Joe Pesci", gender: "Male", age: 79 },
 ]
-app.get('/tickets',(req,res)=>{
+app.get('/tickets', (req, res) => {
     res.send(tickets)
 })
-app.get('/tickets/:id',(req,res)=>{
-    res.send(tickets[req.params.id])
+/*app.get('/tickets/:id',(req,res)=>{
+    if(typeof tickets[req.params.id -1]==='undefined'){
+        return res.status(404).send({error:"ticket not found"})
+    }
+    res.send(tickets[req.params.id -1])
+})*/
+
+app.get("/tickets/:id", (req, res) => {
+    if (!(parseInt(req.params.id) > 0)) {
+        return res.status(400).send({ error: "ID must be a positive integer" })
+    }
+    let result = tickets.find(x => x.id === parseInt(req.params.id))
+    if(typeof(result)==='undefined'){
+        res.status(404).send({error:"Ticket not found."})
+    }
+    res.send(result)
 })
 
-app.get('/locations',(req,res)=>{
+
+
+app.get('/locations', (req, res) => {
     res.send(locations)
 })
 
-app.get('/locations/:id',(req,res)=>{
-    res.send(locations[req.params.id])
+
+app.get("/locations/:id", (req, res) => {
+    if (!(parseInt(req.params.id) > 0)) {
+        return res.status(400).send({ error: "ID must be a positive integer" })
+    }
+    let result = locations.find(x => x.id === parseInt(req.params.id))
+    if(typeof(result)==='undefined'){
+        res.status(404).send({error:"Location not found."})
+    }
+    res.send(result)
 })
 
-app.get('/actors',(req,res)=>{
+
+app.get('/actors', (req, res) => {
     res.send(actors)
 })
-app.get('/actors/:id',(req,res)=>{
-    res.send(actors[req.params.id])
+
+
+app.get("/actors/:id", (req, res) => {
+    if (!(parseInt(req.params.id) > 0)) {
+        return res.status(400).send({ error: "ID must be a positive integer" })
+    }
+    let result = actors.find(x => x.id === parseInt(req.params.id))
+    if(typeof(result)==='undefined'){
+        res.status(404).send({error:"Actor not found."})
+    }
+    res.send(result)
 })
 
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`API up at: http://localhost${port}`);
 })
