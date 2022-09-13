a3de17e38fa49de8d614d643d2160867b83f1ca6
Henry Lehis <lehishenry1@gmail.com>
2022-09-13 10:47:17 +0300

    Ticket Details [Delivers #183186742]

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ const port = 8080
 const swaggerUi = require ('swagger-ui-express')
 const swaggerDocument = require ('./docs/swagger.json');
 
+const tickets = [
+    {id: 1, name: "Top Gun", cast: "Tom Cruise, Jennifer Connelly, Jon Hamm", price: 15.99},
+    {id: 2, name: "The Invitation", cast: "Nathalie Emmanuel, Thomas Doherty", price: 13.99},
+    {id: 3, name: "Avatar", cast: "Sam Worthington, Zoe Saldana, Sigourney Weaver", price: 10.99},
+    {id: 4, name: "André Rieu in Dublin", cast: "André Rieu", price: 25.99},
+    {id: 5, name: "Cinema Classics: Goodfellas", cast: "Robert De Niro, Joe Pesci, Ray Liota", price: 15.99},
+    {id: 6, name: "Beast", cast: "Idris Elba, Sharlto Copley", price: 19.99},
+    {id: 7, name: "Bullet Train", cast: "Brad Pitt, Aaron Taylor-Johnson, Sandra Bullock, Hiroyuki Sanada", price: 11.99},
+    {id: 8, name: "About Fate", cast: "Emma Roberts, Thomas Mann", price: 18.99},
+    {id: 9, name: "After Ever Happy", cast: "Hero Fiennes Tiffin, Josephine Langford, Louise Lombard", price: 16.99},
+    {id: 10, name: "Nope", cast: "Barbie Ferreira, Keke Palmer, Michael Wincott", price: 12.99},
+]
+
 app.get('/tickets',(req,res)=>{
-    res.send(["Concerts","Opera"])
+    res.send(tickets)
+})
+app.get('/tickets/:id',(req,res)=>{
+    res.send(tickets[req.params.id])
 })
 
 app.get('/locations',(req,res)=>{
