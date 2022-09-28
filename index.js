0d468d76a3debaae15a0b9142e6374593e543140
Henry <lehishenry1@gmail.com>
2022-09-28 13:14:09 +0300

    Get a list of tickets [Delivers #183361559] [Delivers #183361504]

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ mongoose.connect("mongodb://localhost:27017/ticketsApiDb")
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 require("./routes/ticketRoutes")(app)
 require("./routes/locationRoutes")(app)
 require("./routes/actorRoutes")(app)
--- tickets-front/src/App.vue
+++ tickets-front/src/App.vue
@@ -1,47 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<h1>Call API</h1>
+<p v-for="item in list" :key="item.id">
+{{item.name}} --- {{item.price}} --- {{item.cast}}
+</p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script>
+import axios from "axios"
+export default {
+  name: "App",
+  data()
+  {
+    return {
+      list:[]
+    }
+  },
+  async mounted()
+  {
+    let result = await axios.get("http://localhost:8080/tickets");
+    console.warn(result.data.data)
+    this.list=result.data
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+};
+</script>
