ad467da3bc3e11a454d761feb403f7f60927cf77
Marko Tagoma <markoets1@gmail.com@>
2022-10-26 08:58:34 +0300

    Make it so user is redirected to login page after signingup

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -67,7 +67,7 @@ exports.signupPage=(req, res) => {
     const user = await User.create({ email, password });
     const token = createToken(user._id);
     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-    res.status(201).json({ user: user._id });
+    res.redirect("login")
   }
   
 
