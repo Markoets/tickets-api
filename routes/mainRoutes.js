a6a38d998028678ace0878e884f96f9628002ae0
Henry <lehishenry1@gmail.com>
2022-11-02 12:20:23 +0200

    Main page changes

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -2,35 +2,36 @@ const mainController = require("../controllers/mainController")
 const { requireAuth, checkUser } = require('../middleware/auth.Middleware');
 const express = require("express");
 const router = express.Router();
- module.exports = function (app) {
- app.get('/ticket', function(req, res) {
-    res.render('ticket',{title:'Express'});
+module.exports = function (app) {
+  app.get('/ticket', function (req, res) {
+    res.render('ticket', { title: 'Express' });
   });
 
-  app.get('/', function(req, res) {
-    res.render('index',{title:'Home'});
+  app.get('/', function (req, res) {
+    res.render('index', { title: 'Home' });
   });
 
-    app.route("/location")
+  app.route("/location")
     .get(mainController.locationsPage)
 
-    app.get('/actor', function(req, res) {
-      res.render('actor',{title:'Express'});
-    });
-   /* app.route("/ticketsadd")
-    .get(mainController.addTickets)*/
+  app.get('/actor', function (req, res) {
+    res.render('actor', { title: 'Express' });
+  });
+
+  /* app.route("/ticketsadd")
+   .get(mainController.addTickets)*/
 
 
-   app.route("/logout")
-   .get(mainController.logout_get)
+  app.route("/logout")
+    .get(mainController.logout_get)
 
 
-   app.get('/ticketsAdd', requireAuth, (req, res) => res.render('ticketsAdd',{title:"Login"}));
+  app.get('/ticketsAdd', requireAuth, (req, res) => res.render('ticketsAdd', { title: "Login" }));
 
-   app.get('/actorsAdd', requireAuth, (req, res) => res.render('actorsAdd',{title:"Login"}));
+  app.get('/actorsAdd', requireAuth, (req, res) => res.render('actorsAdd', { title: "Login" }));
 
-   app.get('/locationsAdd', requireAuth, (req, res) => res.render('locationsAdd',{title:"Login"}));
-   
-   app.get('/admin', requireAuth, (req, res) => res.render('admin',{title:"Login"}));
+  app.get('/locationsAdd', requireAuth, (req, res) => res.render('locationsAdd', { title: "Login" }));
 
- }
+  app.get('/admin', requireAuth, (req, res) => res.render('admin', { title: "Login" }));
+
+}
